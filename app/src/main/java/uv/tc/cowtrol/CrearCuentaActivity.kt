a3890a46b8dd7d7187b0226de539466f9add268c
mosterTeco <reyesmls11@hotmail.com>
2024-05-24 19:45:45 -0600

    Actividad Crear Cuenta

--- app/src/main/java/uv/tc/cowtrol/CrearCuentaActivity.kt
+++ app/src/main/java/uv/tc/cowtrol/CrearCuentaActivity.kt
@@ -10,5 +10,6 @@ class CrearCuentaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crear_cuenta)
+        // .....
     }
 }
